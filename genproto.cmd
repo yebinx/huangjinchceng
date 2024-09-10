fcbae502a2014faa6f488f59db8604cf74e18d6f
yebinx <yebinx@21cn.com>
2024-09-10 19:17:38 +0800

    888

--- genproto.cmd
+++
@@ -1,4 +0,0 @@
-start protoc --proto_path=./proto ^
---plugin=protoc-gen-ts_proto=E:/game/slot/node_modules/.bin/protoc-gen-ts_proto.cmd ^
---ts_proto_opt=esModuleInterop=true ^
---ts_proto_out=paths=source_relative:./genproto 
